420a269d76bd8c8c998bac615a8c89c64478bd58
Vladyslav Kucher <vladkucher75755@gmail.com>
2024-08-13 21:26:19 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -137,54 +137,21 @@ const courseObject = {
 };
 
 const categoriesFromApi = {
-  contentCategory: {
-    Bezpieczenstwo: 1,
-    NawykiZywieniowe: 29,
-    ZdrowieIFitness: 52,
-  },
-  formOfKnowledge: {
-    MaterialyOnline: 6,
-    IndywidualneWsparcie: 4,
-    SocialLearningWymianaWiedzyMiedzyUczestnikami: 7,
-  },
-  groupSize: {
-    DlaPar: 1,
-    Indywidualne: 3,
-    Grupowe: 2,
-  },
-  levelOfAdvancement: {
-    Zaawansowany: 3,
-    Podstawowy: 2,
-    Ekspercki: 1,
-  },
-  softSkills: {
-    ZarzadzanieCzasem: 33,
-    ZwiekczeniePewnosciSiebie: 36,
-  },
-  targetGroup: {
-    OsobyPoszukujaceSpecjalistycznychDiet: 47,
-    EntuzjasciMedycynyNaturalnejISuplementacji: 6,
-    Mlodziez13_18Lat: 26,
-  },
-  trainingMode: {
-    ProduktFizyczny: 3,
-    Stacjonarnie: 4,
-    Hybrydowo: 1,
-    OnLine: 2,
-  },
-  typeOfContent: {
-    Audyt: 1,
-    Ksiazki: 2,
-    Podcasty: 3,
-  }
+  contentCategory: {},
+  formOfKnowledge: {},
+  groupSize: {},
+  levelOfAdvancement: {},
+  softSkills: {},
+  targetGroup: {},
+  trainingMode: {},
+  typeOfContent: {},
 };
 
 const App = () => {
   const [course, setCourse] = useState(courseObject);
   const [tokenToAdd, setTokenToAdd] = useState('');
-  const [categoryFromApi, setCategoryFromApi] = useState(categoriesFromApi);
 
-  const updateCourseWithApiCategories = () => {
+  const updateCourseWithApiCategories = (catFromApi) => {
     const mapApiCategories = (apiCategory) => {
       return Object.keys(apiCategory).map((key) => ({
         id: apiCategory[key],
@@ -194,14 +161,14 @@ const App = () => {
 
     setCourse((prevCourse) => ({
       ...prevCourse,
-      contentCategory: mapApiCategories(categoryFromApi.contentCategory),
-      formOfKnowledge: mapApiCategories(categoryFromApi.formOfKnowledge),
-      groupSize: mapApiCategories(categoryFromApi.groupSize),
-      levelOfAdvancement: mapApiCategories(categoryFromApi.levelOfAdvancement),
-      softSkills: mapApiCategories(categoryFromApi.softSkills),
-      targetGroupList: mapApiCategories(categoryFromApi.targetGroup),
-      typeOfContentList: mapApiCategories(categoryFromApi.typeOfContent),
-      trainingMode: mapApiCategories(categoryFromApi.trainingMode),
+      contentCategory: mapApiCategories(catFromApi.contentCategory),
+      formOfKnowledge: mapApiCategories(catFromApi.formOfKnowledge),
+      groupSize: mapApiCategories(catFromApi.groupSize),
+      levelOfAdvancement: mapApiCategories(catFromApi.levelOfAdvancement),
+      softSkills: mapApiCategories(catFromApi.softSkills),
+      targetGroupList: mapApiCategories(catFromApi.targetGroup),
+      typeOfContentList: mapApiCategories(catFromApi.typeOfContent),
+      trainingMode: mapApiCategories(catFromApi.trainingMode),
     }));
   };
 
@@ -243,7 +210,7 @@ const App = () => {
             checked={!!course[key].find((i) => i.id === item.id)?.checked}
             onChange={(e) => handleCheckboxChange(e, key, item)}
           />
-          {item.name}
+          <span className='ml-0.5'>{item.name}</span>
         </label>
       </div>
     ));
@@ -251,7 +218,7 @@ const App = () => {
 
   async function getAllCategories() {
     const url = 'https://api.prorokszkoleniowy.pl/get-all-categories';
-  
+
     try {
       const response = await fetch(url, {
         method: 'GET',
@@ -260,15 +227,13 @@ const App = () => {
           'Authorization': `Bearer ${tokenToAdd}`,
         },
       });
-  
+
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
-  
+
       const data = await response.json();
-      console.log(data); // Handle the response data as needed
-      setCategoryFromApi(data);
-      updateCourseWithApiCategories();
+      updateCourseWithApiCategories(data);
       return data;
     } catch (error) {
       console.error('Error fetching categories:', error);
@@ -276,134 +241,164 @@ const App = () => {
     }
   }
 
-  useEffect(() => {
-  }, [])
+  async function updateCourse() {
+    const url = 'https://api.prorokszkoleniowy.pl/add-course';
+  
+    try {
+      const response = await fetch(url, {
+        method: 'PATCH',
+        headers: {
+          'accept': '*/*',
+          'Authorization': `Bearer ${tokenToAdd}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(course),
+      });
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+  
+      const data = await response.json();
+      console.log('Course updated successfully:', data);
+      return data;
+    } catch (error) {
+      console.error('Error updating course:', error);
+      throw error;
+    }
+  }
 
   return (
     <div className='w-[1000px] mx-auto my-12'>
       <h1 className='text-4xl mb-4'>Add course form</h1>
-      <div className='bg-lightBeige p-2 w-max flex gap-4 mb-6 border rounded border-maroon max-w-[1000px]'>
-        <label>Token</label>
-        <textarea value={tokenToAdd} onChange={handleChangeToken} type="text" className='w-[900px]' rows={5} />
+      <div className='bg-lightBeige p-2 w-max flex gap-4 mb-6 border-2 rounded border-bronze max-w-[1000px]'>
+        <label className='font-semibold text-xl'>Token</label>
+        <textarea value={tokenToAdd} onChange={handleChangeToken} type="text" className='w-[900px] p-1' rows={3} />
       </div>
-      <button onClick={() => getAllCategories()}>Click</button>
+      <p className='font-light'>Click button below to load categories from api</p>
+      <button
+        onClick={() => getAllCategories()}
+        className='bg-bronze py-2 px-4 rounded text-white mb-4'
+      >
+        Load data
+      </button>
       <form onSubmit={handleSubmit}>
-        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border rounded border-maroon flex-wrap max-w-[1000px]'>
-          <label>Name</label>
-          <input type="text" name="name" value={course.name} onChange={handleChange} />
+        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border-2 rounded border-bronze flex-wrap max-w-[1000px]'>
+          <label className='font-semibold text-xl'>name</label>
+          <input className='p-1' type="text" name="name" value={course.name} onChange={handleChange} />
         </div>
 
-        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border rounded border-maroon flex-wrap max-w-[1000px]'>
-          <label>Site Link</label>
-          <input type="text" name="siteLink" value={course.siteLink} onChange={handleChange} />
+        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border-2 rounded border-bronze flex-wrap max-w-[1000px]'>
+          <label className='font-semibold text-xl'>siteLink</label>
+          <input className='p-1' type="text" name="siteLink" value={course.siteLink} onChange={handleChange} />
         </div>
 
-        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border rounded border-maroon flex-wrap max-w-[1000px]'>
-          <label>Course Link</label>
-          <input type="text" name="courseLink" value={course.courseLink} onChange={handleChange} />
+        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border-2 rounded border-bronze flex-wrap max-w-[1000px]'>
+          <label className='font-semibold text-xl'>courseLink</label>
+          <input className='p-1' type="text" name="courseLink" value={course.courseLink} onChange={handleChange} />
         </div>
 
-        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border rounded border-maroon flex-wrap max-w-[1000px]'>
-          <label>Type of Content</label>
-          {renderCheckboxList('typeOfContentList', course.trainingMode)}
+        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border-2 rounded border-bronze flex-wrap max-w-[1000px]'>
+          <label className='font-semibold text-xl'>typeOfContentList</label>
+          {renderCheckboxList('typeOfContentList', course.typeOfContentList)}
         </div>
 
-        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border rounded border-maroon flex-wrap max-w-[1000px]'>
-          <label>Target Group</label>
+        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border-2 rounded border-bronze flex-wrap max-w-[1000px]'>
+          <label className='font-semibold text-xl'>targetGroupList</label>
           {renderCheckboxList('targetGroupList', course.targetGroupList)}
         </div>
 
-        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border rounded border-maroon flex-wrap max-w-[1000px]'>
-          <label>Creation Date</label>
-          <input type="datetime-local" name="creationDate" value={course.creationDate} onChange={handleChange} />
+        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border-2 rounded border-bronze flex-wrap max-w-[1000px]'>
+          <label className='font-semibold text-xl'>creationDate</label>
+          <input className='p-1' type="datetime-local" name="creationDate" value={course.creationDate} onChange={handleChange} />
         </div>
 
-        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border rounded border-maroon flex-wrap max-w-[1000px]'>
-          <label>Creator's Contact</label>
-          <input type="text" name="creatorsContact" value={course.creatorsContact} onChange={handleChange} />
+        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border-2 rounded border-bronze flex-wrap max-w-[1000px]'>
+          <label className='font-semibold text-xl'>creatorsContact</label>
+          <input className='p-1' type="text" name="creatorsContact" value={course.creatorsContact} onChange={handleChange} />
         </div>
 
-        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border rounded border-maroon flex-wrap max-w-[1000px]'>
-          <label>Min Price</label>
-          <input type="number" name="minPrice" value={course.minPrice} onChange={handleChange} />
+        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border-2 rounded border-bronze flex-wrap max-w-[1000px]'>
+          <label className='font-semibold text-xl'>minPrice</label>
+          <input className='p-1' type="number" name="minPrice" value={course.minPrice} onChange={handleChange} />
         </div>
 
-        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border rounded border-maroon flex-wrap max-w-[1000px]'>
-          <label>Max Price</label>
-          <input type="number" name="maxPrice" value={course.maxPrice} onChange={handleChange} />
+        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border-2 rounded border-bronze flex-wrap max-w-[1000px]'>
+          <label className='font-semibold text-xl'>maxPrice</label>
+          <input className='p-1' type="number" name="maxPrice" value={course.maxPrice} onChange={handleChange} />
         </div>
 
-        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border rounded border-maroon flex-wrap max-w-[1000px]'>
-          <label>Rating on Google</label>
-          <input type="number" name="ratingOnGoogle" value={course.ratingOnGoogle} onChange={handleChange} />
+        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border-2 rounded border-bronze flex-wrap max-w-[1000px]'>
+          <label className='font-semibold text-xl'>ratingOnGoogle</label>
+          <input className='p-1' type="number" name="ratingOnGoogle" value={course.ratingOnGoogle} onChange={handleChange} />
         </div>
 
-        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border rounded border-maroon flex-wrap max-w-[1000px]'>
-          <label>Marketing Description</label>
-          <textarea name="marketingDescription" value={course.marketingDescription} onChange={handleChange} />
+        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border-2 rounded border-bronze flex-wrap max-w-[1000px]'>
+          <label className='font-semibold text-xl'>marketingDescription</label>
+          <textarea className='p-1' name="marketingDescription" value={course.marketingDescription} onChange={handleChange} />
         </div>
 
-        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border rounded border-maroon flex-wrap max-w-[1000px]'>
-          <label>For AI Description</label>
-          <textarea name="forAIDescription" value={course.forAIDescription} onChange={handleChange} />
+        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border-2 rounded border-bronze flex-wrap max-w-[1000px]'>
+          <label className='font-semibold text-xl'>forAIDescription</label>
+          <textarea className='p-1' name="forAIDescription" value={course.forAIDescription} onChange={handleChange} />
         </div>
 
-        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border rounded border-maroon flex-wrap max-w-[1000px]'>
-          <label>Training Mode</label>
+        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border-2 rounded border-bronze flex-wrap max-w-[1000px]'>
+          <label className='font-semibold text-xl'>trainingMode</label>
           {renderCheckboxList('trainingMode', course.trainingMode)}
         </div>
 
-        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border rounded border-maroon flex-wrap max-w-[1000px]'>
-          <label>Content Category</label>
+        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border-2 rounded border-bronze flex-wrap max-w-[1000px]'>
+          <label className='font-semibold text-xl'>contentCategory</label>
           {renderCheckboxList('contentCategory', course.contentCategory)}
         </div>
 
-        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border rounded border-maroon flex-wrap max-w-[1000px]'>
-          <label>Benefits of Course</label>
-          <textarea name="benefitsOfCourse" value={course.benefitsOfCourse} onChange={handleChange} />
+        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border-2 rounded border-bronze flex-wrap max-w-[1000px]'>
+          <label className='font-semibold text-xl'>benefitsOfCourse</label>
+          <textarea className='p-1' name="benefitsOfCourse" value={course.benefitsOfCourse} onChange={handleChange} />
         </div>
 
-        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border rounded border-maroon flex-wrap max-w-[1000px]'>
-          <label>Training Topics</label>
-          <textarea name="trainingTopics" value={course.trainingTopics} onChange={handleChange} />
+        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border-2 rounded border-bronze flex-wrap max-w-[1000px]'>
+          <label className='font-semibold text-xl'>trainingTopics</label>
+          <textarea className='p-1' name="trainingTopics" value={course.trainingTopics} onChange={handleChange} />
         </div>
 
-        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border rounded border-maroon flex-wrap max-w-[1000px]'>
-          <label>Start Date</label>
-          <input type="datetime-local" name="startDate" value={course.startDate} onChange={handleChange} />
+        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border-2 rounded border-bronze flex-wrap max-w-[1000px]'>
+          <label className='font-semibold text-xl'>startDate</label>
+          <input className='p-1' type="datetime-local" name="startDate" value={course.startDate} onChange={handleChange} />
         </div>
 
-        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border rounded border-maroon flex-wrap max-w-[1000px]'>
-          <label>Group Size</label>
+        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border-2 rounded border-bronze flex-wrap max-w-[1000px]'>
+          <label className='font-semibold text-xl'>groupSize</label>
           {renderCheckboxList('groupSize', course.groupSize)}
         </div>
 
-        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border rounded border-maroon flex-wrap max-w-[1000px]'>
-          <label>Soft Skills</label>
+        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border-2 rounded border-bronze flex-wrap max-w-[1000px]'>
+          <label className='font-semibold text-xl'>softSkills</label>
           {renderCheckboxList('softSkills', course.softSkills)}
         </div>
 
-        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border rounded border-maroon flex-wrap max-w-[1000px]'>
-          <label>Form of Knowledge</label>
+        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border-2 rounded border-bronze flex-wrap max-w-[1000px]'>
+          <label className='font-semibold text-xl'>formOfKnowledge</label>
           {renderCheckboxList('formOfKnowledge', course.formOfKnowledge)}
         </div>
 
-        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border rounded border-maroon flex-wrap max-w-[1000px]'>
-          <label>Become After Training</label>
-          <textarea name="becomeAfterTraining" value={course.becomeAfterTraining} onChange={handleChange} />
+        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border-2 rounded border-bronze flex-wrap max-w-[1000px]'>
+          <label className='font-semibold text-xl'>becomeAfterTraining</label>
+          <textarea className='p-1' name="becomeAfterTraining" value={course.becomeAfterTraining} onChange={handleChange} />
         </div>
 
-        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border rounded border-maroon flex-wrap max-w-[1000px]'>
-          <label>Level of Advancement</label>
+        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border-2 rounded border-bronze flex-wrap max-w-[1000px]'>
+          <label className='font-semibold text-xl'>levelOfAdvancement</label>
           {renderCheckboxList('levelOfAdvancement', course.levelOfAdvancement)}
         </div>
 
-        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border rounded border-maroon flex-wrap max-w-[1000px]'>
-          <label>Image ID</label>
-          <input type="number" name="imageId" value={course.imageId} onChange={handleChange} />
+        <div className='bg-lightBeige p-2 w-max flex gap-4 mb-2 border-2 rounded border-bronze flex-wrap max-w-[1000px]'>
+          <label className='font-semibold text-xl'>imageId</label>
+          <input className='p-1' type="number" name="imageId" value={course.imageId} onChange={handleChange} />
         </div>
 
-        <button className='bg-maroon py-2 px-4 rounded text-white' type="submit">Submit</button>
+        <button className='bg-bronze py-2 px-4 rounded text-white' type="submit" onClick={() => updateCourse()}>Submit</button>
       </form>
     </div>
   );
